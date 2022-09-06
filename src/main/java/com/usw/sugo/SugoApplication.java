6ac5596b8317c5a1613b23fa5300c180d5a8e851
K-Diger <tigerkingdh991009@gmail.com>
2022-09-06 15:25:43 +0900

    init : 프로젝트 초기화

---
+++ src/main/java/com/usw/sugo/SugoApplication.java
@@ -0,0 +1,13 @@
+package com.usw.sugo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SugoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SugoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/usw/sugo/SugoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usw.sugo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SugoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
