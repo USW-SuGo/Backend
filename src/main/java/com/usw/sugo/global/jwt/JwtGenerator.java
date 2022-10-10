cc9f5c47ca7a341295a91be350083babf05819bf
K-Diger <tigerkingdh991009@gmail.com>
2022-10-10 16:33:52 +0900

    fix : JSON Import 변경 springconfiguration -> org.json

--- src/main/java/com/usw/sugo/global/jwt/JwtGenerator.java
+++ src/main/java/com/usw/sugo/global/jwt/JwtGenerator.java
@@ -2,7 +2,6 @@ package com.usw.sugo.global.jwt;
 
 import com.usw.sugo.domain.majoruser.User;
 import com.usw.sugo.domain.majoruser.user.repository.UserDetailsRepository;
-import com.usw.sugo.domain.majoruser.user.repository.UserRepository;
 import com.usw.sugo.domain.refreshtoken.RefreshToken;
 import com.usw.sugo.domain.refreshtoken.repository.RefreshTokenRepository;
 import io.jsonwebtoken.*;
