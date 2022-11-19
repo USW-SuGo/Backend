f8025c23777905644f44471a3c6f0af2a6715a13
K-Diger <kitten.diger@gmail.com>
2022-11-19 20:45:24 +0900

    refactor : 채팅 관련 도메인 제거

--- src/main/java/com/usw/sugo/domain/chatting/room/repository/ChattingRoomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.usw.sugo.domain.chatting.room.repository;
-
-import com.usw.sugo.domain.chatting.entity.ChattingRoom;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChattingRoomRepository extends JpaRepository<ChattingRoom, Long>, CustomChattingRoomRepository {
-}
