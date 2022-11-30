965ce95fb366c8fd716b9b2f5d2fbe546165782a
K-Diger <kitten.diger@gmail.com>
2022-11-30 14:58:21 +0900

    chore : logging

--- src/main/java/com/usw/sugo/SugoApplication.java
+++ src/main/java/com/usw/sugo/SugoApplication.java
@@ -1,8 +1,11 @@
 package com.usw.sugo;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -13,11 +16,22 @@ import javax.persistence.EntityManager;
 @EnableScheduling
 @SpringBootApplication
 public class SugoApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(SugoApplication.class, args);
-	}
-	@Bean
-	public JPAQueryFactory jpaQueryFactory(EntityManager em) {
-		return new JPAQueryFactory(em);
-	}
+
+    private static final Logger logger = LoggerFactory.getLogger(SugoApplication.class);
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(EntityManager em) {
+        return new JPAQueryFactory(em);
+    }
+
+    public static void main(String[] args) {
+        try (ConfigurableApplicationContext ctx = SpringApplication.run(SugoApplication.class, args)) {
+            logger.error("error log");
+            logger.warn("warn log");
+            logger.info("info log");
+            logger.debug("debug log");
+            logger.trace("trace log");
+        }
+    }
+
 }
