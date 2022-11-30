73c3d747ea0d600d24b7993a120b50f5b9d60f93
K-Diger <kitten.diger@gmail.com>
2022-11-30 15:02:15 +0900

    chore : logging

--- src/main/java/com/usw/sugo/SugoApplication.java
+++ src/main/java/com/usw/sugo/SugoApplication.java
@@ -5,7 +5,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -25,13 +24,7 @@ public class SugoApplication {
     }
 
     public static void main(String[] args) {
-        try (ConfigurableApplicationContext ctx = SpringApplication.run(SugoApplication.class, args)) {
-            logger.error("error log");
-            logger.warn("warn log");
-            logger.info("info log");
-            logger.debug("debug log");
-            logger.trace("trace log");
-        }
+        SpringApplication.run(SugoApplication.class, args);
     }
 
 }
