abedde915ec7a65c14edeeff02414fa925899ec4
K-Diger <kitten.diger@gmail.com>
2023-01-30 03:06:54 +0900

    refactor : AuthenticationFilter 간소화

--- src/main/java/com/usw/sugo/global/security/authentication/UserDetailsServiceImpl.java
+++ src/main/java/com/usw/sugo/global/security/authentication/UserDetailsServiceImpl.java
@@ -3,8 +3,6 @@ package com.usw.sugo.global.security.authentication;
 import com.usw.sugo.domain.user.User;
 import com.usw.sugo.domain.user.repository.UserDetailsRepository;
 import com.usw.sugo.global.exception.CustomException;
-import com.usw.sugo.global.exception.ExceptionType;
-
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -12,8 +10,11 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 
+import static com.usw.sugo.global.exception.ExceptionType.USER_NOT_EXIST;
+
 
 @Service
+@Transactional(readOnly = true)
 public class UserDetailsServiceImpl implements UserDetailsService {
     private final UserDetailsRepository userRepository;
 
@@ -21,11 +22,10 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         this.userRepository = userRepository;
     }
 
-    @Transactional
     @Override
     public UserDetailsImpl loadUserByUsername(String loginId) throws UsernameNotFoundException {
         User user = userRepository.findByLoginId(loginId)
-                .orElseThrow(() -> new CustomException(ExceptionType.USER_NOT_EXIST));
-        return new UserDetailsImpl(user, user.getStatus().toString(), new ArrayList<>());
+                .orElseThrow(() -> new CustomException(USER_NOT_EXIST));
+        return new UserDetailsImpl(user, user.getStatus(), new ArrayList<>());
     }
 }
--- src/main/java/com/usw/sugo/global/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/usw/sugo/global/security/filter/JwtAuthenticationFilter.java
@@ -1,19 +1,6 @@
 package com.usw.sugo.global.security.filter;
 
 import com.usw.sugo.global.exception.CustomException;
-import com.usw.sugo.global.exception.ExceptionType;
-import com.usw.sugo.global.jwt.JwtResolver;
-import com.usw.sugo.global.jwt.JwtValidator;
-import com.usw.sugo.global.security.authentication.CustomAuthenticationManager;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -21,86 +8,37 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.NoSuchElementException;
+import java.util.List;
 
-import static com.usw.sugo.global.exception.ExceptionType.*;
+import static com.usw.sugo.global.exception.ExceptionType.REQUIRE_TOKEN;
 
 /*
 매 요청마다 JWT 가 유효한지 검증하고, 유효할 시 해당 유저에 Security Context 를 인가 해주는 필터
  */
-@RequiredArgsConstructor
-@Slf4j
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    private final UserDetailsService userDetailsService;
-    private final CustomAuthenticationManager authenticationManager;
-    private final JwtResolver jwtResolver;
-    private final JwtValidator jwtValidator;
+    private final List<String> whiteListURI = List.of(
+            "/user/check-email", "/user/check-loginId", "/user/auth", "/user/join", "/user/login",
+            "/user/find-id", "/user/find-pw", "/post/all", "/token");
 
     @Override
-    protected void doFilterInternal(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            FilterChain filterChain) throws ServletException, IOException {
-
-        // AccessToken이 필요없는 요청 필터링 - 시작
-        String[] whiteListURI = {
-                "/user/check-email", "/user/check-loginId", "/user/login",
-                "/user/auth", "/user/join",
-                "/user/find-id", "/user/find-pw",
-                "/post/all",
-                "/token"
-        };
-        for (String whiteList : whiteListURI) {
-            if (request.getRequestURI().equals(whiteList)) {
-                filterChain.doFilter(request, response);
-                return;
-            }
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        if (validateHeader(request) || validateURI(request)) {
+            filterChain.doFilter(request, response);
         }
+    }
 
-        if (request.getHeader("Authorization") == null) {
+    private boolean validateHeader(HttpServletRequest request) {
+        if (request.getHeader("Authorization") == null ||
+                !request.getHeader("Authorization").contains("Bearer ")) {
             throw new CustomException(REQUIRE_TOKEN);
         }
+        return true;
+    }
 
-        String token = request.getHeader("Authorization").substring(7);
-        jwtValidator.validateToken(token);
-
-        String loginId = jwtResolver.jwtResolveToUserLoginId(token);
-
-        // AccessToken 에 담긴 정보가 DB 에 존재하는 유저일 때
-        try {
-            UserDetails requestUserDetails = userDetailsService.loadUserByUsername(loginId);
-            System.out.println("LoginId 조회 1");
-        } catch (CustomException | NoSuchElementException e) {
-            JSONObject responseJson = new JSONObject();
-            try {
-                responseJson.put("code", HttpServletResponse.SC_BAD_REQUEST);
-                responseJson.put("message", ExceptionType.USER_NOT_EXIST.toString());
-            } catch (JSONException ex) {
-                throw new RuntimeException(ex);
-            }
-            response.setContentType("application/json;charset=UTF-8");
-            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            response.getWriter().print(responseJson);
-            response.flushBuffer();
-            return;
-        }
-
-        UserDetails requestUserDetails = userDetailsService.loadUserByUsername(loginId);
-        System.out.println("LoginId 조회 2");
-
-        // JWT 를 바탕으로 인증 객체 생성
-        Authentication authToken = new UsernamePasswordAuthenticationToken(
-                requestUserDetails.getUsername(), requestUserDetails.getPassword());
-
-        // Authorities 부여
-        Authentication auth = authenticationManager.authenticate(authToken);
-
-        // SecurityContextHolder 에 저장
-        SecurityContextHolder.getContext().setAuthentication(auth);
-
-        filterChain.doFilter(request, response);
+    private boolean validateURI(HttpServletRequest request) {
+        return whiteListURI.stream()
+                .noneMatch(whiteListURI::contains);
     }
 }
-
-
