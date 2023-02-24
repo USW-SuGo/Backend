a616391aeb1af6fe6f587ab3a0fa7efc99d82362
K-Diger <kitten.diger@gmail.com>
2023-02-24 20:17:56 +0900

    chore : 성능 테스트 마무리 후 비동기 스레드 할당 셋팅 복원

--- src/main/java/com/usw/sugo/global/async/AsyncConfig.java
+++ src/main/java/com/usw/sugo/global/async/AsyncConfig.java
@@ -1,24 +1,24 @@
-//package com.usw.sugo.global.async;
-//
-//import java.util.concurrent.Executor;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.scheduling.annotation.AsyncConfigurer;
-//import org.springframework.scheduling.annotation.EnableAsync;
-//import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-//
-//@Configuration
-//@EnableAsync
-//public class AsyncConfig implements AsyncConfigurer {
-//
-//    @Override
-//    public Executor getAsyncExecutor() {
-//        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-//        executor.setCorePoolSize(1);
-//        executor.setMaxPoolSize(10);
-//        executor.setQueueCapacity(30);
-//        executor.setThreadNamePrefix("SUGO-DIGER-ASYNC-");
-//        executor.initialize();
-//
-//        return executor;
-//    }
-//}
+package com.usw.sugo.global.async;
+
+import java.util.concurrent.Executor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig implements AsyncConfigurer {
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(1);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(30);
+        executor.setThreadNamePrefix("SUGO-DIGER-ASYNC-");
+        executor.initialize();
+
+        return executor;
+    }
+}
