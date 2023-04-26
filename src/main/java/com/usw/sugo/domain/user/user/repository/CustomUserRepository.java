794df049cc35cfcba4118b51b7bc54523fb93391
K-Diger <kitten.diger@gmail.com>
2023-04-26 16:37:17 +0900

    fix : deleteByUser 메서드 추가

--- src/main/java/com/usw/sugo/domain/user/user/repository/CustomUserRepository.java
+++ src/main/java/com/usw/sugo/domain/user/user/repository/CustomUserRepository.java
@@ -1,9 +1,12 @@
 package com.usw.sugo.domain.user.user.repository;
 
+import com.usw.sugo.domain.user.user.User;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CustomUserRepository {
 
     String findToAvailableNicknameNumber(String department);
+
+    void deleteByUser(User user);
 }
--- src/main/java/com/usw/sugo/domain/user/user/repository/CustomUserRepositoryImpl.java
+++ src/main/java/com/usw/sugo/domain/user/user/repository/CustomUserRepositoryImpl.java
@@ -3,6 +3,8 @@ package com.usw.sugo.domain.user.user.repository;
 import static com.usw.sugo.domain.user.user.QUser.user;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.usw.sugo.domain.user.user.QUser;
+import com.usw.sugo.domain.user.user.User;
 import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -23,5 +25,13 @@ public class CustomUserRepositoryImpl implements CustomUserRepository {
             .orderBy(user.nickname.desc())
             .limit(1)
             .fetchOne();
+    }
+
+    @Override
+    public void deleteByUser(User inputUser) {
+        queryFactory
+            .delete(user)
+            .where(user.id.eq(inputUser.getId()))
+            .execute();
     }
 }
